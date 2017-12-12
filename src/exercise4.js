6a35ecf6c5e8e6cd09e4feacd69c73ce2b4bc74f
Yanis DANDO <yanis.dando@gmail.com>
2017-12-12 17:12:07 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,7 +1,11 @@
+//return a particular string :
+//"The value of One is 1, The value of Two is 2,
+// The value of Three is 3".
 exports.run = function(input) {
-  console.log("Exercise #4");
 
-  // YOUR CODE IS WELCOME HERE
+  var myFunction = () =>{
+    return "The value of "+input.argv[2][1]+" is "+input.argv[1][1]+", The value of "+input.argv[2][2]+" is "+input.argv[1][2]+", The value of "+input.argv[2][3]+" is "+input.argv[1][3]+""
+  }
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
