40d43846876cb30e361db1f9a44395819ea5736d
Yanis DANDO <yanis.dando@gmail.com>
2017-12-12 17:13:07 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -7,6 +7,8 @@ exports.run = function(input) {
     return "The value of "+input.argv[2][1]+" is "+input.argv[1][1]+", The value of "+input.argv[2][2]+" is "+input.argv[1][2]+", The value of "+input.argv[2][3]+" is "+input.argv[1][3]+""
   }
 
+  //Not a matrice?
+
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
   /** */
