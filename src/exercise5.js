d7dbc8fd13cf69a63b15bca1c5399476b82c457c
Yanis DANDO <yanis.dando@gmail.com>
2017-12-12 17:06:06 +0100

    Exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,19 @@
+//return (a number) which is the sum of the values written in the path given in input.
+var fs = require('fs');
+
 exports.run = function(input) {
-  console.log("Exercise #5");
+  var size = 0;
+
+  var path = fs.readFileSync(input);
+  // console.log(path.toString());
+    var dot = path.toString();
+    // console.log(dot.toString());
+
+    for (var i = 0; i < dot.length; i++) {
+      size += Number(dot[i]);
+    }
+    // console.log(size);
+
+  return size;
+
 };
