49cf1d9a0f7727dd4047e77edfdf28e33b1dcbf7
Yanis DANDO <yanis.dando@gmail.com>
2017-12-12 16:03:16 +0100

    Exercise 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,12 @@
+//return a string with only number under 6 of the string given in input.
 exports.run = function(input) {
-  console.log("Exercise #2");
+  var tab = [];
+  var res = [];
+  tab = input.split("");
+  for (var i=0; i<input.length ; i++) {
+    if (tab[i] < 6) {
+      res.push(tab[i]);
+    }
+  }
+  return res.join('');
 };
