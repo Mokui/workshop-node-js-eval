b35eea31176d122eee53be55b19b114e6ccfab9e
Yanis DANDO <yanis.dando@gmail.com>
2017-12-12 16:14:49 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,20 @@
-exports.run = function(input) {
-  console.log("Exercise #3");
+//return (a number) which is the result of the values of objects
+//with specific operator mentionned in the property "operator".
+// For example +2-4+3=1
+// run([
+//   { operator: 'add', value: 2 },
+//   { operator: 'substract', value: 4 },
+//   { operator: 'add', value: 3 }
+// ]) // 1
+exports.run = function(run) {
+  var val = 0;
+  for (var i = 0; i < run.length; i++) {
+    if(run[i].operator === "add"){
+        val += run[i].value;
+    }
+    if(run[i].operator === "substract"){
+        val -= run[i].value;
+    }
+  }
+  return val;
 };
